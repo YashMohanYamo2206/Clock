34cb35e8a4f8480a194fe349966ed3160602b54f
Yash Mohan <yashmohan2206@gmail.com>
2020-08-22 17:16:41 +0530

    alert dialog

--- app/src/main/java/com/yash/clock/Alerts_and_Notifications/AlertReceiver.java
+++ app/src/main/java/com/yash/clock/Alerts_and_Notifications/AlertReceiver.java
@@ -12,7 +12,18 @@ import androidx.core.app.NotificationCompat;
 public class AlertReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+        dialog.setMessage("QUESTION GOES HERE");
+        dialog.setTitle("INSTRUCTION");
+        dialog.setPositiveButton("GOTCHA.!!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
+            }
+        });
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.setCancelable(false);
+        alertDialog.show();
         //Toast.makeText(context, "123", Toast.LENGTH_SHORT).show();
     }
 }
